b0e1d01df7ccbafcefc0130124b17b8992675bc4
Andrew Brooking <atbrooking.dev@gmail.com>
2020-01-03 16:31:33 -0600

    Starting conversion to React

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Portfolio
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/Components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from "@material-ui/core"
+
+function About() {
+  return (
+    <Container className="dark-bg light-text">
+        <span>ANDREW BROOKING</span>
+    </Container>
+  );
+}
+
+export default About;
